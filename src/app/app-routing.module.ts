443594d1023fcf48fc34edc27d0f2b02de962207
Cordz <joshuabombitacorda@gmail.com>
2024-02-18 21:39:32 +0800

    tanga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'contacts', component: FooterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
